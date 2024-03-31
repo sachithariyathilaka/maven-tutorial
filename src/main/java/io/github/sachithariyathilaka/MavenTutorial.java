8b5eee3da49545c3ff17e469072f0e44df2f3bb6
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-03-31 16:54:45 +0530

    Code refactor

--- src/main/java/io/github/sachithariyathilaka/MavenTutorial.java
+++ src/main/java/io/github/sachithariyathilaka/MavenTutorial.java
@@ -7,7 +7,7 @@ package io.github.sachithariyathilaka;
  * @version 1.0.0
  * @since ``2024/03/16
  */
-public class Main {
+public class MavenTutorial {
 
     public static void main(String[] args) {
         System.out.println("Maven tutorial project working!");
